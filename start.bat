59d9a391f1379aa895406b4dc914f5662ce76acc
UMMADISETTIVAMSI <your-email@example.com>
2026-07-22 10:20:24 +0530

    add features branch

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting SmartAttendanceQR...
 
-start cmd /k "cd /d d:\desktop\SmartAttendanceQR\backend && mvn spring-boot:run"
+start cmd /k "set JAVA_HOME=C:\Program Files\Java\jdk-21.0.11 && set PATH=%JAVA_HOME%\bin;%PATH% && cd /d d:\desktop\SmartAttendanceQR\backend && mvn spring-boot:run"
 timeout /t 20 /nobreak
 start cmd /k "cd /d d:\desktop\SmartAttendanceQR\frontend && npm run dev"
 
